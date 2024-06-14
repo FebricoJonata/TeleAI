e6e9919aa88a806f010bf6a5d6c941329c74a542
Nucizz <calvinanacia123@gmail.com>
2024-06-15 00:56:12 +0700

    Added textfield and button component

---
+++ src/components/BaseButton.jsx
@@ -0,0 +1,28 @@
+export default function BaseButton({
+    title, 
+    action, 
+    prominence = 'primary',
+    isFullWidth = false,
+    ...rest
+}) {
+    return (
+        <button 
+        className={`${handleProminenceClass(prominence)} ${isFullWidth ? 'w-full' : ''} ${rest}`} 
+        onClick={action}>
+            {title}
+        </button>
+    );
+}
+
+function handleProminenceClass(prominence) {
+    var baseClass = 'text-[18px] px-[32px] py-[8px] transition-all duration-300 h-min rounded-[10px] '
+    if (prominence === 'secondary') {
+        baseClass += 'bg-neutral-low hover:bg-neutral-med text-white border-white border-[1px]'
+    }  else if (prominence === 'tertiary') {
+        baseClass += 'text-white hover:text-brand-primary'
+    } else { // Default primary
+        baseClass += 'bg-brand-primary hover:bg-brand-secondary text-white'
+    }
+
+    return baseClass
+}
