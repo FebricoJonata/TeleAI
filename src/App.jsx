3acc00c775b8a4a1077c0269d0555913f546362a
Fabian <fabian.djaja@gmail.com>
2024-06-14 20:33:44 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
+import ChatbotPage from "./pages/chatbot";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-sky-900">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ChatbotPage />} />
+          {/* <Route path="/MyShop" element={<MyShop />} />
+          <Route path="/detail/:gameId" element={<GameDetail />} />
+          <Route path="/UploadItems" element={<UploadItems />} />
+          <Route path="/Library" element={<LibraryPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+     
     </>
   );
 }
---
+++ src/pages/chatbot.jsx
@@ -0,0 +1,8 @@
+const ChatbotPage = ()=>{
+    return(
+        <div className="h-full w-full" style={{background: 'linear-gradient(180deg, #000428 0%, #004e92 100%)'}}>
+          <h1>tes</h1>
+        </div>
+    );
+}
+export default ChatbotPage
