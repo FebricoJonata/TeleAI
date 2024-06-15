b5ad5d8934d2d5861e2c0c20c3c3b3c05ab14b16
FebricoJonata <febrico.jonata@kawanlamaretail.com>
2024-06-15 10:07:32 +0700

    fix: url public

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,36 +4,38 @@ export default {
   theme: {
     extend: {},
     colors: {
-      'brand': {
-        'primary': '#7B53FF', //Purple
-        'secondary': '#6230FF', //Purple Pink
-        'inv': '#181721', //Dark Blue
-        'semi-inv': '#1817219D', //Semi Transparant Dark Blue
+      brand: {
+        primary: "#7B53FF", //Purple
+        secondary: "#6230FF", //Purple Pink
+        inv: "#181721", //Dark Blue
+        "semi-inv": "#1817219D", //Semi Transparant Dark Blue
       },
-      'neutral': {
-        'med': '#FFFFFF4D', //Semi Transparant White
-        'low': '#FFFFFF1A', //Semi Transparant White
-        'blacklow':"#181721B3" 
-      }
-      
-    }
+      neutral: {
+        med: "#FFFFFF4D", //Semi Transparant White
+        low: "#FFFFFF1A", //Semi Transparant White
+        blacklow: "#181721B3",
+      },
+    },
   },
   plugins: [
     function ({ addUtilities }) {
-      addUtilities({
-        '.no-scrollbar::-webkit-scrollbar': {
-          display: 'none',
+      addUtilities(
+        {
+          ".no-scrollbar::-webkit-scrollbar": {
+            display: "none",
+          },
+          ".no-scrollbar": {
+            "-ms-overflow-style": "none",
+            "scrollbar-width": "none",
+          },
+          ".bg-brand-blurry": {
+            backgroundImage: "url('./bgImage.svg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          },
         },
-        '.no-scrollbar': {
-          '-ms-overflow-style': 'none',
-          'scrollbar-width': 'none',
-        },
-        '.bg-brand-blurry': {
-          'backgroundImage': "url('./src/assets/bgImage.svg')",
-          'backgroundSize': 'cover',
-          'backgroundPosition': 'center'
-        }
-      }, ['responsive', 'hover']);
+        ["responsive", "hover"]
+      );
     },
   ],
 };
